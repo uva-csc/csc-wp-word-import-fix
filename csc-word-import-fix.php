6eed14a2078fb064ea890d1cc52faaf7088c4c6d
ThanGrove <than@virginia.edu>
2025-06-12 13:02:41 -0400

    adding button to change editor content

--- csc-word-import-fix.php
+++ csc-word-import-fix.php
@@ -5,12 +5,24 @@
  * Version: 1.0
  */
 
-add_action('enqueue_block_editor_assets', function () {
-    wp_enqueue_script(
-        'csc-word-import-fix',
-        plugin_dir_url(__FILE__) . 'js/csc-word-import-fix.js',
-        ['wp-blocks', 'wp-element', 'wp-editor', 'wp-data', 'wp-plugins', 'wp-edit-post'],
-        null,
-        true
-    );
-});
+function csc_word_import_fix_enqueue_editor_script( $hook ) {
+    // Run only on post editing screens
+    if ( in_array( $hook, [ 'post.php', 'post-new.php' ], true ) ) {
+        // Import JS for plugin
+        wp_enqueue_script(
+            'csc-word-import-fix',
+            plugin_dir_url(__FILE__) . 'js/csc-word-import-fix.js',
+            ['wp-blocks', 'wp-element', 'wp-editor', 'wp-data', 'wp-plugins', 'wp-edit-post', 'jquery'],
+            null,
+            true
+        );
+        // Import CSS for plugin
+        wp_enqueue_style(
+            'csc-word-import-fix-css',
+            plugin_dir_url( __FILE__ ) . 'csc-word-import-fix.css',
+            [],
+            '1.0'
+        );
+    }
+}
+add_action( 'admin_enqueue_scripts', 'csc_word_import_fix_enqueue_editor_script' );
--- js/csc-word-import-fix.js
+++ js/csc-word-import-fix.js
@@ -1,31 +1,100 @@
-const { createElement } = wp.element;
-const { PluginToolbarButton } = wp.editPost;
-const { registerPlugin } = wp.plugins;
-const { select, dispatch } = wp.data;
-const { parse } = wp.blocks;
 
-const FixBlockHtmlButton = () => {
-    const fixBlockHtml = () => {
-        const rawHTML = select('core/editor').getEditedPostContent();
-        console.log("Raw html is: \n", rawHTML);
-        const newBlocks = rawHTML; // parse(rawHTML); // Converts HTML to Gutenberg blocks
-        dispatch('core/block-editor').resetBlocks(newBlocks); // Replace existing blocks
-    };
+const { select, subscribe, dispatch } = wp.data;
 
-    return createElement(
-        PluginToolbarButton,
-        {
-            icon: 'editor-code',
-            label: 'Convert HTML to Blocks',
-            onClick: fixBlockHtml
+console.log("csc word import fix hhh");
+
+const doIt = (e) => {
+    console.log("doing it 4 u!");
+    const iframe = jQuery('.block-editor-iframe__scale-container iframe');
+    console.log(iframe);
+    iframe.addClass('greyed-out');
+    let content;
+    const unsubscribe = subscribe(() => {
+        content = select('core/editor').getEditedPostAttribute('content');
+        //console.log("content in subscribe", content);
+        if (content) {
+            unsubscribe();
+            console.log('Updating content');
+            content = content.replace('<!-- wp:paragraph -->\n' +
+                '<p></p>\n' +
+                '<!-- /wp:paragraph -->', '');
+            content += '<!-- wp:paragraph --><p>Newly 2 added HTML content</p><!-- /wp:paragraph -->';
+            dispatch('core/editor').editPost({
+                content: content,
+            });
+            console.log("removing grey");
+            iframe.removeClass('greyed-out');
         }
-    );
-};
-console.log(wp.editPost);
-if (wp.editPost && wp.editPost.PluginToolbarButton) {
-    console.log('PluginToolbarButton is:', PluginToolbarButton);
-    registerPlugin('csc-word-import-fix-plugin', {
-        render: FixBlockHtmlButton,
-        icon: 'editor-code'
     });
 }
+
+const doItnew = () => {
+    console.log("doing it 4 u!");
+    const iframe = jQuery('.block-editor-iframe__scale-container iframe');
+    iframe.addClass('greyed-out');
+
+    const { select, dispatch, subscribe } = wp.data;
+
+    const unsubscribe = subscribe(() => {
+        const isReady = select('core/editor').isSavingPost() === false &&
+            select('core/editor').isEditedPostDirty() !== undefined;
+
+        const content = select('core/editor').getEditedPostAttribute('content');
+
+        if (isReady && content !== null) {
+            console.log('Editor is ready, updating content');
+            unsubscribe();
+
+            let updatedContent = content.replace(
+                '<!-- wp:paragraph -->\n<p></p>\n<!-- /wp:paragraph -->',
+                ''
+            );
+            updatedContent += '<!-- wp:paragraph --><p>Added HTML content</p><!-- /wp:paragraph -->';
+
+            dispatch('core/editor').editPost({ content: updatedContent });
+
+            iframe.removeClass('greyed-out');
+        }
+    });
+};
+
+
+setTimeout(() => {
+    const header = document.querySelector('.edit-post-header');
+    console.log("got the header again 3", header);
+    const settingsdiv = jQuery('.editor-header__settings');
+    console.log(settingsdiv);
+    settingsdiv.prepend('<button type="button" aria-pressed="false" ' +
+        'class="components-button is-compact has-icon" onclick="doIt()">' +
+        '<img src="/wp-content/plugins/simple-plugin-test/wrench.svg" title="Fix it" alt="Wrench icon" />' +
+        '</button>');
+}, 1000);
+
+/*
+
+const unsubscribe = subscribe(() => {
+    let content = select('core/editor').getEditedPostAttribute('content');
+    if (content) {
+        console.log('Post content loaded NEW~:', content);
+        unsubscribe(); // Only run once
+        setTimeout(() => {
+            const header = document.querySelector('.edit-post-header');
+            console.log("header", header);
+            const settingsdiv = jQuery('.editor-header__settings');
+            console.log(settingsdiv);
+            settingsdiv.prepend('<button type="button" aria-pressed="false" ' +
+                'class="components-button is-compact has-icon" onclick="doIt()">' +
+                '<img src="/wp-content/plugins/simple-plugin-test/wrench.svg" title="Fix it" alt="Wrench icon" />' +
+                '</button>')
+            /!*
+            if (header && !header.querySelector('.my-custom-fix-button')) {
+                const button = document.createElement('button');
+                button.className = 'components-button is-secondary my-custom-fix-button';
+                button.innerText = 'Fix';
+                button.style.marginLeft = '8px';
+                button.onclick = () => alert('Fix button clicked!');
+                header.appendChild(button);
+            }*!/
+        }, 1000);
+    }
+});*/
